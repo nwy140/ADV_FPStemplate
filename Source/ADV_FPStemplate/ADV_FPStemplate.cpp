675eb50826375ed03204eaf0638aaa5a282a1d4d
SInt-Win11 <throwawayspectacle@gmail.com>
2023-02-10 13:48:23 +0800

    CPP

---
+++ Source/ADV_FPStemplate/ADV_FPStemplate.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ADV_FPStemplate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ADV_FPStemplate, "ADV_FPStemplate" );
